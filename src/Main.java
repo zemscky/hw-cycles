850a0a0df6115e364caf03a9f4403e02cb4ef1a1
zemscky <aleksei.zemscky@yandex.ru>
2022-09-22 13:18:17 +0300

    Домашнее задание 2, задание 2

--- src/Main.java
+++ src/Main.java
@@ -29,5 +29,10 @@ public class Main {
         for (int y=1904; y<=2096; y+=4) {
             System.out.println(y + " год является високосным");
         }
+        // Задание 2.2
+        System.out.println("Задание 2.2");
+        for (int z=7;z<=98;z+=7){
+            System.out.println(z);
+        }
     }
 }
