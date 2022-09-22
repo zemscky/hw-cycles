e32ac289b143134f406f747f47f87c83853664a3
zemscky <aleksei.zemscky@yandex.ru>
2022-09-22 13:50:55 +0300

    Домашнее задание 3, задание 1

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,51 @@ public class Main {
         System.out.println("Домашнее задание 1");
         // Задание 1.1
         System.out.println("Задание 1.1");
-        for (int i = 1; i<=10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         // Задание 2.1
         System.out.println("Задание 2.1");
-        for (int j = 10; j>=1; j--) {
+        for (int j = 10; j >= 1; j--) {
             System.out.println(j);
         }
         // Задание 3.1
         System.out.println("Задание 3.1");
-        for (int f = 0; f<=17; f+=2) {
+        for (int f = 0; f <= 17; f += 2) {
             System.out.println(f);
         }
         // Задание 4.1
         System.out.println("Задание 4.1");
-        for (int x = 10; x>=-10;x-=1) {
+        for (int x = 10; x >= -10; x -= 1) {
             System.out.println(x);
         }
         // Домашнее задание 2
         System.out.println("Домашнее задание 2");
         // Задание 1.2
         System.out.println("Задание 1.2");
-        for (int y=1904; y<=2096; y+=4) {
+        for (int y = 1904; y <= 2096; y += 4) {
             System.out.println(y + " год является високосным");
         }
         // Задание 2.2
         System.out.println("Задание 2.2");
-        for (int z=7;z<=98;z+=7){
+        for (int z = 7; z <= 98; z += 7) {
             System.out.println(z);
         }
         // Задание 3.2
         System.out.println("Задание 3.2");
-        for (int e=1;e<=512;e*=2) {
+        for (int e = 1; e <= 512; e *= 2) {
             System.out.println(e);
         }
+        // Домашнее задание 3
+        System.out.println("Домашнее задание 3");
+        // Задание 1.3
+        System.out.println("Задание 1.3");
+        int moneyPerMonth = 29000;
+        int total = 0;
+        for (int m = 1; m<12;m+=1) {
+            total = total + moneyPerMonth;
+            System.out.println("Месяц " + m + ", сумма накоплений равна " +total+ " рублей");
+        }
+        System.out.println(total);
     }
 }
