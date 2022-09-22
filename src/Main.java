f37311f15c857b96c1d7ec64de01ca11c8dc4cba
zemscky <aleksei.zemscky@yandex.ru>
2022-09-22 12:59:04 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
-        System.out.println("Задание 1");
+        // Задание 1.1
+        System.out.println("Задание 1.1");
         for (int i = 1; i<=10; i++) {
             System.out.println(i);
         }
-        // Задание 2
-        System.out.println("Задание 2");
+        // Задание 2.1
+        System.out.println("Задание 2.1");
         for (int j = 10; j>=1; j--) {
             System.out.println(j);
+        }
+        // Задание 3.1
+        System.out.println("Задание 3.1");
+        for (int f = 0; f<=17; f+=2) {
+            System.out.println(f);
         }
     }
 }
