1f89c471fd32f33d236a1551ba65424e54cb3d65
zemscky <aleksei.zemscky@yandex.ru>
2022-09-22 14:00:30 +0300

    Домашнее задание 3, задание 2

--- src/Main.java
+++ src/Main.java
@@ -45,10 +45,19 @@ public class Main {
         System.out.println("Задание 1.3");
         int moneyPerMonth = 29000;
         int total = 0;
-        for (int m = 1; m<12;m+=1) {
+        for (int m = 1; m <= 12; m += 1) {
             total = total + moneyPerMonth;
-            System.out.println("Месяц " + m + ", сумма накоплений равна " +total+ " рублей");
+            System.out.println("Месяц " + m + ", сумма накоплений равна " + total + " рублей");
         }
         System.out.println(total);
+        // Задание 2.3
+        System.out.println("Задание 2.3");
+        int moneyPerMonth1 = 29000;
+        int total1 = 0;
+        for (int m = 1; m <= 12; m += 1) {
+            total1 = total1 + total1 /100;
+            total1 = total1 + moneyPerMonth1;
+            System.out.println("Месяц " + m + ", сумма накоплений равна " + total1 + " рублей");
+        }
     }
 }
